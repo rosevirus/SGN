5f403cdbe1e8b7c23681739666419488b52c5edd
SirGonny <rosevirus.k@gmail.com>
2019-03-15 18:33:57 +0900

    [solideo-001] add board

--- routes/index.js
+++ routes/index.js
@@ -3,21 +3,21 @@ var router = express.Router();
 var template = require('../lib/template.js');
 var mysql = require('mysql');
 
+//db connect
 var db = mysql.createConnection({
   host: '127.0.0.1',
   user: 'root',
   password: 'tiger',
   database: 'solideo'
 });
-
 db.connect();
 
 router.get('/', (req, res) => {
   db.query('SELECT * FROM sldo_notice', (error, result) => {
     var sldonotice = template.notice(result);
-    var main = template.main(sldonotice);
+    var body = template.home(sldonotice);
     var title = template.title('');
-    var html = template.html(title, main);
+    var html = template.html(title, body);
     res.send(html);
   });
 });
